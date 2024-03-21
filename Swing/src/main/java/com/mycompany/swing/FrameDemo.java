f96d68cfd2c888aab5070052fbdc8392f8797ac7
adnanalemran <adnanalemranontor@gmail.com>
2024-03-21 20:00:46 +0600

    set loctaion

--- Swing/src/main/java/com/mycompany/swing/FrameDemo.java
+++ Swing/src/main/java/com/mycompany/swing/FrameDemo.java
@@ -9,5 +9,6 @@ public class FrameDemo {
         frame.setSize(400, 300); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         frame.setVisible(true);
+        frame.setLocation(200,50);
     }
 }
