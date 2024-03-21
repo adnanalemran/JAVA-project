582c6e5b0be8b7d7f04aad216dbfaa4eaab01cb4
adnanalemran <adnanalemranontor@gmail.com>
2024-03-21 19:46:57 +0600

    visiable fream

--- Swing/src/main/java/com/mycompany/swing/FrameDemo.java
+++ Swing/src/main/java/com/mycompany/swing/FrameDemo.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.swing;
 
-/**
- *
- * @author ADNAN
- */
+import javax.swing.JFrame;
+
 public class FrameDemo {
     
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 300); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);
+    }
 }
