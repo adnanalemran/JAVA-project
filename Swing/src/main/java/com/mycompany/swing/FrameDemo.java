a1fb44a220198545b1056316557930e91f8cd4d1
adnanalemran <adnanalemranontor@gmail.com>
2024-03-23 20:02:25 +0600

    update code

--- Swing/src/main/java/com/mycompany/swing/FrameDemo.java
+++ Swing/src/main/java/com/mycompany/swing/FrameDemo.java
@@ -29,14 +29,15 @@ public class FrameDemo extends JFrame {
 
     }
 
+
     public static void main(String[] args) {
         FrameDemo frame = new FrameDemo();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        // frame.setSize(400, 300);
-        // frame.setLocation(200,50);
-        frame.setBounds(200, 50, 400, 300);
+        frame.setSize(400, 300);
+        frame.setLocation(200,50);
+        // frame.setBounds(200, 50, 400, 300);
         frame.setTitle("This a title");
         frame.setResizable(false);
     }
