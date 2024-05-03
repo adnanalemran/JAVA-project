0929f41931fb47480c913a498536afe3dbf89d6e
adnanalemran <adnanalemranontor@gmail.com>
2024-05-03 23:49:41 +0600

    5 : Assignment 1 | create class & print data

--- basice/src/main/java/com/example/BioData.java
+++ basice/src/main/java/com/example/BioData.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class BioData {
     public static void main(String[] args) {
-    System.out.println("hello");
+    System.out.println("MAdnan ");
     }
 
 }
---
+++ basice/src/main/java/com/example/Product.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Product {
+    public static void main(String[] args) {
+
+        System.out.println("id:101");
+        System.out.println("title: iphone13");
+        System.out.println("price:1895 eurs");
+        System.out.println("description: prefact product with best image");
+        System.out.println("catagory: phone");
+    }
+
+}
