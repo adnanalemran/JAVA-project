cdb2c5e1e66afa486eeb76f64db8de95ef174c72
adnanalemran <adnanalemranontor@gmail.com>
2024-05-03 23:59:36 +0600

    7 : Comments | Single line | Multiple line

--- basice/src/main/java/com/example/BioData.java
+++ basice/src/main/java/com/example/BioData.java
@@ -2,7 +2,14 @@ package com.example;
 
 public class BioData {
     public static void main(String[] args) {
+
         System.out.println("My  Name is Adnan \nMy nack name : ontor \t i am study BSc in CSE ");
+
+        /*
+         * multi line comment
+         */
+
+        // single line commend
 
     }
 
