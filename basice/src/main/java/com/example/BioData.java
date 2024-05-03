d910239635a4f753ef037dc5ea3ddfb71b6de23a
adnanalemran <adnanalemranontor@gmail.com>
2024-05-03 23:56:41 +0600

    6 : Escape sequence

--- basice/src/main/java/com/example/BioData.java
+++ basice/src/main/java/com/example/BioData.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class BioData {
     public static void main(String[] args) {
-    System.out.println("MAdnan ");
+        System.out.println("My  Name is Adnan \nMy nack name : ontor \t i am study BSc in CSE ");
+
     }
 
 }
