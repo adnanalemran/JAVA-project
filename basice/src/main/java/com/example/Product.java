64ed36d9ba1844a27c34d8c31259c49b6c25c491
adnanalemran <adnanalemranontor@gmail.com>
2024-05-04 00:33:31 +0600

    11 : Assignment 2 | Variables and data types

--- basice/src/main/java/com/example/Product.java
+++ basice/src/main/java/com/example/Product.java
@@ -2,12 +2,17 @@ package com.example;
 
 public class Product {
     public static void main(String[] args) {
+        int id = 101;
+        String title = "iphone15";
+        String price = "1895 euros";
+        String description = "perfect product with best image quality";
+        String category = "phone";
 
-        System.out.println("id:101");
-        System.out.println("title: iphone13");
-        System.out.println("price:1895 eurs");
-        System.out.println("description: prefact product with best image");
-        System.out.println("catagory: phone");
+        System.out.println("id is: " + id);
+        System.out.println("Title is: " + title);
+        System.out.println("Price is: " + price);
+        System.out.println("Description is: " + description);
+        System.out.println("Category is: " + category);
     }
 
 }
